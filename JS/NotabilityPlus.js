3e1c94d791b8ca033c4a57fce47fd250eb406f01
vkm64 <118407341+vkm64@users.noreply.github.com>
2022-11-27 20:07:58 -0500

    Update NotabilityPlus.js

--- JS/NotabilityPlus.js
+++ JS/NotabilityPlus.js
@@ -1,3 +1,29 @@
-let obj = JSON.parse($response.body);
-obj = {"data":{"processAppleReceipt":{"__typename":"SubscriptionResult","error":0,"subscription":{"__typename":"AppStoreSubscription","status":"active","originalPurchaseDate":"2021-11-09T03:14:18.000Z","originalTransactionId":"710000869822929","expirationDate":"2099-12-31T03:14:17.000Z","productId":"com.gingerlabs.Notability.premium_subscription","tier":"premium","refundedDate":null,"refundedReason":null,"user":null}}}};
+var obj = JSON.parse($response.body);
+
+obj = 
+
+{
+  "data": {
+    "processAppleReceipt": {
+      "__typename": "SubscriptionResult",
+      "error": 0,
+      "subscription": {
+        "__typename": "AppStoreSubscription",
+        "status": "active",
+        "originalPurchaseDate": "2022-10-31T23:35:29.000Z",
+        "originalTransactionId": "300001262346134",
+        "expirationDate": "2099-12-31T23:35:25.000Z",
+        "productId": "com.gingerlabs.Notability.premium_subscription",
+        "tier": "premium",
+        "refundedDate": null,
+        "refundedReason": null,
+        "isInBillingRetryPeriod": false,
+        "gracePeriodExpiresAt": null,
+        "expirationIntent": null,
+        "overDeviceLimit": false,
+        "user": null
+      }
+    }
+  }
+}
 $done({body: JSON.stringify(obj)});
