0075cf8db2ae349c08b76a2d8911ede1c558cd35
vkm64 <118407341+vkm64@users.noreply.github.com>
2022-12-01 19:45:02 -0500

    Update BCR.js

--- JS/BCR.js
+++ JS/BCR.js
@@ -6,7 +6,7 @@ obj =
   "app_id" : "de74abc4-e524-44ff-a161-37e2f8de4384",
   "net_type" : 1,
   "ios_bundle" : "com.abbyy.bcrfree",
-  "timezone_id" : "Europe/Madrid",
+  "timezone_id" : "EN_CA",
   "tags" : {
     "bcr.ItemsInCardHolder" : 0,
     "com.abbyy.bcrfree" : "1",
@@ -17,7 +17,7 @@ obj =
     "bcr.IsPromocodeActive" : "0",
     "bcr.ABBYYCloudSyncOn" : "",
     "bcr.IsPremium" : "1",
-    "DeviceID" : "5918a4d9-6371-4544-b25d-4b8aa22916e2",
+    "DeviceID" : "5AFCC946-C4EC-4685-B572-76AA6C25E37C",
     "IsTestVersion" : "0",
     "com.abbyy.textgrabber" : "1",
     "com.abbyy.bcr2" : "1",
@@ -27,12 +27,12 @@ obj =
   "sdk_type" : "030700",
   "sdk" : "030700",
   "identifier" : "8c9a9c09e36d6c494856d2b8321883e8617ec71d592b08b8b1ec3ec99774d98a",
-  "language" : "es-ES",
+  "language" : "en-EN",
   "device_os" : "16.1.1",
-  "game_version" : "9.8.4",
+  "app_version" : "9.8.4",
   "timezone" : 3600,
   "notification_types" : 0,
-  "device_model" : "iPhone13,4",
-  "carrier" : "Vodafone"
+  "device_model" : "iPhone15,3",
+  "carrier" : "TELUS"
 }
 $done({body: JSON.stringify(obj)});
