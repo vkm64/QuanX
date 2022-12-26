5a75ebf965a02e78f88989fef3c878c73ce51850
vkm64 <118407341+vkm64@users.noreply.github.com>
2022-12-25 20:12:44 -0500

    Update Filterra.js

--- JS/Filterra.js
+++ JS/Filterra.js
@@ -4,7 +4,7 @@ var objc = JSON.parse($response.body);
 {
   "receipt" : {
     "receipt_type" : "Production",
-    "app_item_id" : 1531983371,
+    "app_item_id" : 951704333,
     "receipt_creation_date" : "2022-10-21 11:08:33 Etc/GMT",
     "bundle_id" : "com.filterra.editor",
     "original_purchase_date" : "2021-12-19 14:47:24 Etc/GMT",
@@ -24,18 +24,18 @@ var objc = JSON.parse($response.body);
         "original_purchase_date" : "2021-12-19 14:51:30 Etc/GMT"
       }
     ],
-    "adam_id" : 1531983371,
+    "adam_id" : 951704333,
     "receipt_creation_date_pst" : "2022-10-21 04:08:33 America/Los_Angeles",
     "request_date" : "2022-10-21 11:09:08 Etc/GMT",
     "request_date_pst" : "2022-10-21 04:09:08 America/Los_Angeles",
-    "version_external_identifier" : 852499820,
+    "version_external_identifier" : 852701403,
     "request_date_ms" : "1666350548068",
     "original_purchase_date_pst" : "2021-12-19 06:47:24 America/Los_Angeles",
-    "application_version" : "182",
+    "application_version" : "3.14.103",
     "original_purchase_date_ms" : "1639925244000",
     "receipt_creation_date_ms" : "1666350513000",
-    "original_application_version" : "100",
-    "download_id" : 500986395327087400
+    "original_application_version" : "3.0.107",
+    "download_id" : 95042525156520
   },
   "environment" : "Production",
   "status" : 0,
