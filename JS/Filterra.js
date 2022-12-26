abb423db400b005d3d2b086ae270e2c4d546bb3b
vkm64 <118407341+vkm64@users.noreply.github.com>
2022-12-25 20:03:11 -0500

    Update Filterra.js

--- JS/Filterra.js
+++ JS/Filterra.js
@@ -4,10 +4,10 @@ var objc = JSON.parse($response.body);
 {
   "receipt" : {
     "receipt_type" : "Production",
-    "app_item_id" : 951704333,
+    "app_item_id" : 1531983371,
     "receipt_creation_date" : "2022-10-21 11:08:33 Etc/GMT",
     "bundle_id" : "com.filterra.editor",
-    "original_purchase_date" : "2019-05-15 02:02:30 Etc/GMT",
+    "original_purchase_date" : "2021-12-19 14:47:24 Etc/GMT",
     "in_app" : [
       {
         "quantity" : "1",
@@ -16,27 +16,46 @@ var objc = JSON.parse($response.body);
         "is_trial_period" : "false",
         "original_transaction_id" : "20000961765202",
         "purchase_date" : "2021-12-19 14:51:30 Etc/GMT",
-        "product_id" : "filterra.clubvipmembership"",
-        "original_purchase_date_pst" : "2019-05-14 19:02:30 America/Los_Angeles",
+        "product_id" : "filterra.clubvipmembership",
+        "original_purchase_date_pst" : "2021-12-19 06:51:30 America/Los_Angeles",
         "in_app_ownership_type" : "PURCHASED",
         "original_purchase_date_ms" : "1639925490000",
         "purchase_date_pst" : "2021-12-19 06:51:30 America/Los_Angeles",
         "original_purchase_date" : "2021-12-19 14:51:30 Etc/GMT"
       }
     ],
-    "adam_id" : 951704333,
-    "receipt_creation_date_pst" : "2022-12-25 15:23:49 America/Los_Angeles",
+    "adam_id" : 1531983371,
+    "receipt_creation_date_pst" : "2022-10-21 04:08:33 America/Los_Angeles",
     "request_date" : "2022-10-21 11:09:08 Etc/GMT",
     "request_date_pst" : "2022-10-21 04:09:08 America/Los_Angeles",
-    "version_external_identifier" : 852701403,
+    "version_external_identifier" : 852499820,
     "request_date_ms" : "1666350548068",
-    "original_purchase_date_pst" : "2019-05-15 02:02:30 America/Los_Angeles",
-    "application_version" : "3.14.103",
+    "original_purchase_date_pst" : "2021-12-19 06:47:24 America/Los_Angeles",
+    "application_version" : "182",
     "original_purchase_date_ms" : "1639925244000",
     "receipt_creation_date_ms" : "1666350513000",
-    "original_application_version" : "3.0.107",
-    "download_id" : 95042525156520
+    "original_application_version" : "100",
+    "download_id" : 500986395327087400
   },
+  "environment" : "Production",
   "status" : 0,
+  "latest_receipt_info" : [
+    {
+      "quantity" : "1",
+      "purchase_date_ms" : "1639925490000",
+      "transaction_id" : "20000961765202",
+      "is_trial_period" : "false",
+      "original_transaction_id" : "20000961765202",
+      "purchase_date" : "2021-12-19 14:51:30 Etc/GMT",
+      "product_id" : "filterra.clubvipmembership",
+      "original_purchase_date_pst" : "2021-12-19 06:51:30 America/Los_Angeles",
+      "in_app_ownership_type" : "PURCHASED",
+      "original_purchase_date_ms" : "1639925490000",
+      "purchase_date_pst" : "2021-12-19 06:51:30 America/Los_Angeles",
+      "original_purchase_date" : "2021-12-19 14:51:30 Etc/GMT"
+    }
+  ],
+  "latest_receipt" : "MCpIMDEwKzAqSDEwMDA0KzAwMTgyMCAwcTAxMDAwW1A2MFAyNTYwMmwweEMtMFByb2R1Y3Rpb24wTDZ1NSxRSDAwNjcwK29Fem16MDIwMjItMTAtMjFUMTE6MDg6MzNaMDIwMjItMTAtMjFUMTE6MDk6MDhaMDIwMjEtMTItMTlUMTQ6NDc6MjRaMCNjb20uZmlsdGVycmEuZWRpdG9yMEA4c2s7bUEgZDtSUFZ3Wz4iIlx8Xn4wQztvZm8jeGBnPy0pLlcpam88SlhhYSkjIjRmSURjND0wbGIxXjAwMDAwMDAwMDAwMDAwXTAyMDAwMDk2MTc2NTIwMjAyMDAwMDk2MTc2NTIwMjAyMDIxLTEyLTE5VDE0OjUxOjMwWjAyMDIxLTEyLTE5VDE0OjUxOjMwWjAlZmlsdGVycmEuY2x1YnZpcG1lbWJlcnNoaXAwfDBkVzAqSDAxMFVVUzEwVUFwcGxlIEluYy4xLDAqVSNBcHBsZSBXb3JsZHdpZGUgRGV2ZWxvcGVyIFJlbGF0aW9uczFEMEJVO0FwcGxlIFdvcmxkd2lkZSBEZXZlbG9wZXIgUmVsYXRpb25zIENlcnRpZmljYXRpb24gQXV0aG9yaXR5MDE1MTExMzAyMTUwOVoyMzAyMDcyMTQ4NDdaMDE3MDVVLk1hYyBBcHAgU3RvcmUgYW5kIGlUdW5lcyBTdG9yZSBSZWNlaXB0IFNpZ25pbmcxLDAqVSNBcHBsZSBXb3JsZHdpZGUgRGV2ZWxvcGVyIFJlbGF0aW9uczEwVUFwcGxlIEluYy4xMFVVUzAiMCpIMCVbIzMjPUpVOVRtfVQsUnFke0lxP1tGLlg+IXhMWypoN0pYUjUtSFh4VGsxNkpHVDd9X0pjKz5vPFIldmtbXnNRSVZDXGlpaDlzXXN2Oy1JIzAwPyszMDEwLyswI2h0dHA6Ly9vY3NwLmFwcGxlLmNvbS9vY3NwMDMtd3dkcjA0MFV2TTZdKzBVMDBVIzAnYEdZUlQwVSAwMCpIY2QwMCswUmVsaWFuY2Ugb24gdGhpcyBjZXJ0aWZpY2F0ZSBieSBhbnkgcGFydHkgYXNzdW1lcyBhY2NlcHRhbmNlIG9mIHRoZSB0aGVuIGFwcGxpY2FibGUgc3RhbmRhcmQgdGVybXMgYW5kIGNvbmRpdGlvbnMgb2YgdXNlLCBjZXJ0aWZpY2F0ZSBwb2xpY3kgYW5kIGNlcnRpZmljYXRpb24gcHJhY3RpY2Ugc3RhdGVtZW50cy4wNisqaHR0cDovL3d3dy5hcHBsZS5jb20vY2VydGlmaWNhdGVhdXRob3JpdHkvMFUwKkhjZDAqSC49WytuQmxDMF9cJVkuNS58a3dnXnZaIGNzZUhcKDo3LTlzKipuKyBcfSczRExnbk9rUVp1ZWJfaGRYYHx1aVsqaG5CTDc7ekNtSGA2Ky0jaXs5R0UwYS8wIjA5bTAqSDBiMTBVVVMxMFVBcHBsZSBJbmMuMVVBcHBsZSBDZXJ0aWZpY2F0aW9uIEF1dGhvcml0eTEwVUFwcGxlIFJvb3QgQ0EwMTMwMjA3MjE0ODQ3WjIzMDIwNzIxNDg0N1owMTBVVVMxMFVBcHBsZSBJbmMuMSwwKlUjQXBwbGUgV29ybGR3aWRlIERldmVsb3BlciBSZWxhdGlvbnMxRDBCVTtBcHBsZSBXb3JsZHdpZGUgRGV2ZWxvcGVyIFJlbGF0aW9ucyBDZXJ0aWZpY2F0aW9uIEF1dGhvcml0eTAiMCpIMDhUViQ5SF80U3p8Y2F3ZF4jOVAtVlhwLX5uSj4vV3tqSSoyY2hSPWt9ZzdqZ1FkXFduZzV2Nm44K057JVpOcWAzTlVYd0VQSCstaGd+cFlLd0I5TDtIIV1qajVgRil5amFnaGJfbmNlMDBVJ2BHWVJUMFUwMFUjMCtpR3ZrLkBHTV4wLlUnMCUwIyFodHRwOi8vY3JsLmFwcGxlLmNvbS9yb290LmNybDBVMCpIY2QwKkhPWSxsLzI1bn1pdyxnbTpZXCphckUwR3RyOz5FVDJTa05QbCFOV2tbPD0jSj9MbVd6WHtoSklVSzApdzQ7YmNBO0xPTSl6Zi4heDhnMFFccWwwMDAqSDBiMTBVVVMxMFVBcHBsZSBJbmMuMVVBcHBsZSBDZXJ0aWZpY2F0aW9uIEF1dGhvcml0eTEwVUFwcGxlIFJvb3QgQ0EwMDYwNDI1MjE0MDM2WjM1MDIwOTIxNDAzNlowYjEwVVVTMTBVQXBwbGUgSW5jLjFVQXBwbGUgQ2VydGlmaWNhdGlvbiBBdXRob3JpdHkxMFVBcHBsZSBSb290IENBMCIwKkgwR1BeeS02V0xVS2wiMD5QQWYkayp6R1s3M01pcl1fJVVNXWQ1I0tZUFhQZyAsIG9wPzBDPStJKF49OiEudDxicUdTVS9BcExFfkxrUEF0YkEzMFhaMmhlc2deZUl2M2V3LXowdjBVMFUwMFUraUd2ay5AR01eMFUjMCtpR3ZrLkBHTV4wVSAwMCpIY2QwMCoraHR0cHM6Ly93d3cuYXBwbGUuY29tL2FwcGxlY2EvMCswUmVsaWFuY2Ugb24gdGhpcyBjZXJ0aWZpY2F0ZSBieSBhbnkgcGFydHkgYXNzdW1lcyBhY2NlcHRhbmNlIG9mIHRoZSB0aGVuIGFwcGxpY2FibGUgc3RhbmRhcmQgdGVybXMgYW5kIGNvbmRpdGlvbnMgb2YgdXNlLCBjZXJ0aWZpY2F0ZSBwb2xpY3kgYW5kIGNlcnRpZmljYXRpb24gcHJhY3RpY2Ugc3RhdGVtZW50cy4wKkhcNkwteHd2dzBPPUc3QCxzZHlPND54a305UyA4T2srWXxAVnQjO0dvJHBFJ214fiI1JWskI3NgWy9ESGA4PTNqL1NqW2RjM3c6LFYhc082VTJCcX5SQiQqTV5jS1A3dXUhMTAwMDEwVVVTMTBVQXBwbGUgSW5jLjEsMCpVI0FwcGxlIFdvcmxkd2lkZSBEZXZlbG9wZXIgUmVsYXRpb25zMUQwQlU7QXBwbGUgV29ybGR3aWRlIERldmVsb3BlciBSZWxhdGlvbnMgQ2VydGlmaWNhdGlvbiBBdXRob3JpdHlXMCswKkhyQT4qM3BVVD5DSHQpUjozfnN0KzRGXDtDelZNPERtXzoicj8iZ3Z6I34iP1RsMj9sOW0oWjJhUiF5RER7aSR2UFpDSWNiM3JtZmZxTTxbRFpzdFJBeVg/YGdXJVo="
 }
+
 $done({body : JSON.stringify(objc)});
