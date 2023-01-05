65cb1f2c74df19b16f86d729bc62109b1b9fd31c
vkm64 <118407341+vkm64@users.noreply.github.com>
2023-01-04 21:19:00 -0500

    Update Brass.js

--- JS/Brass.js
+++ JS/Brass.js
@@ -1,3 +1,6 @@
+var obj = JSON.parse($response.body);
+
+obj = 
 {
   "request_date_ms" : 1672746605724,
   "request_date" : "2023-01-03T11:50:05Z",
@@ -8,25 +11,19 @@
     "other_purchases" : {},
     "management_url" : null,
     "subscriptions" : {
-         "brass.pro.annual": {
-                    "unsubscribe_detected_at": null,
-                    "expires_date":"2099-09-28T09:00:00Z",
-                    "is_sandbox": false,
-                    "refunded_at": null,
-                    "auto_resume_date": null,
-                    "grace_period_expires_date": null,
-                    "period_type":"premium",
-                    "purchase_date":"2023-01-03T11:50:05Z",
-                    "billing_issues_detected_at": null,
-                    "ownership_type":"PURCHASED",
-                    "store": "app_store",
-                    "original_purchase_date":"2021-01-31T18:15:46Z",
+        "brass.pro.annual": {
+    "unsubscribe_detected_at": null,               "expires_date":"2099-09-28T09:00:00Z",
+    "is_sandbox": false,
+    "refunded_at": null,
+    "auto_resume_date": null,                      "grace_period_expires_date": null,
+    "period_type":"premium",                       "purchase_date":"2023-01-03T11:50:05Z",        "billing_issues_detected_at": null,            "ownership_type":"PURCHASED",
+    "store": "app_store",                    "original_purchase_date":"2021-01-31T18:15:46Z",
          }
     },
     "entitlements" : {
-         "pro": {
-                    "expires_date":"2999-09-28T09:00:00Z",
-                    "purchase_date":"2023-01-03T11:50:05Z",
+         "pro"
+        {  
+         "expires_date":"2999-09-28T09:00:00Z"                  "purchase_date":"2023-01-03T11:50:05Z",
                     "product_identifier"Brass_Premium
                     "grace_period_expires_date": null
          }
